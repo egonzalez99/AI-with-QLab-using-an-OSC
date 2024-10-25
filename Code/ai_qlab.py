e529ca497a18345a7a47c6894b7e27c9842e7071
egonzalez99 <112592570+egonzalez99@users.noreply.github.com>
2024-10-25 00:43:40 -0400

    updated the pdf and mp4 to open

--- Code/ai_qlab.py
+++ Code/ai_qlab.py
@@ -1,6 +1,7 @@
 from playsound import playsound
 import cv2
 import tkinter as tk
+import pdfplumber
 from tkinter import filedialog
 from PIL import Image
 
@@ -15,16 +16,25 @@ def load_media() :
     
 def mediaplayer(file_path):
     # open these type of text files 
-    if file_path.endswith(".txt") or file_path.endswith(".pdf"):
-        
+    if file_path.endswith(".txt"):
         try:
             with open(file_path, 'r') as f:
                 text_content = f.read()
-                print("Text content(s): ")
+                print("Text content(s):")
                 print(text_content)
+        except Exception as e:
+            print(f"Error reading text file: {e}")
+    
+    elif file_path.endswith(".pdf"):
+        try:
+            # Using pdfplumber to read the PDF content
+            with pdfplumber.open(file_path) as pdf:
+                print("PDF content(s):")
+                for page in pdf.pages:
+                    print(page.extract_text())
                 
         except Exception as e:
-            print(f"Error reading file: {e}")
+            print(f"Error for PDF file: {e}")
     
     # play an audio file
     elif file_path.endswith('.mp3') or file_path.endswith('.wav'):
@@ -50,8 +60,14 @@ def mediaplayer(file_path):
     # this will play a video
     elif file_path.endswith('.mp4') or file_path.endswith('.avi'):
         try:
+            
+            #open the video with opencv
             cap = cv2.VideoCapture(file_path)
-            while True:
+            if not cap.isOpened():
+                print("Error: Couldn't open video file.")
+                return
+            
+            while cap.isOpened():
                 ret, frame = cap.read()
                 
                 if not ret:
@@ -60,7 +76,7 @@ def mediaplayer(file_path):
                 cv2.imshow('video', frame)
                 
                 # quit the video with 'q' key after 1 mills
-                if cv2.waitKey(1.5) & 0xFF == ord('q'):
+                if cv2.waitKey(25) & 0xFF == ord('q'):
                     break
                 
             cap.release()  
